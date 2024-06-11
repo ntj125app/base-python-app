216d36e0d63ac42bab98440824b60559032eec0d
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-06-11 09:56:13 +0000

    FInish base python API

--- app/main.py
+++ app/main.py
@@ -18,32 +18,43 @@ sentry_sdk.init(
 app = FastAPI()
 
 # Health check API
+
+
 @app.get("/app/healthcheck")
 def read_root():
     return JSONResponse({"status": "ok"})
 
 # Celery list worker API
+
+
 @app.get("/api/workers")
 def get_workers():
     workers = celery.control.inspect().stats()
     return JSONResponse(workers)
 
 # Celery get registered tasks API
+
+
 @app.get("/api/tasks")
 def get_task_lists():
     tasks = celery.control.inspect().registered()
     return JSONResponse(tasks)
 
 # Celery get list of active tasks API
+
+
 @app.get("/api/active-tasks")
 def get_active_tasks():
     tasks = celery.control.inspect().active()
     return JSONResponse(tasks)
 
 # Submit task API
+
+
 class TaskModel(BaseModel):
     task_name: str
     task_args: dict | str | None = None
+
 
 @app.post("/api/task-submit")
 def submit_task(args: TaskModel | None):
@@ -53,6 +64,8 @@ def submit_task(args: TaskModel | None):
     return JSONResponse({"task_id": task.id, "task_name": args.task_name, "task_args": argsDict})
 
 # Task status API
+
+
 @app.get("/api/task-status/{task_id}")
 def get_task_status(task_id):
     task = AsyncResult(task_id)
