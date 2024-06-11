7fef1531ef45b8c60bc58f2a0739ac51fcbb6a07
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-06-11 06:22:10 +0000

    Basic app with PG support

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Body
 import sentry_sdk
 import os
 from fastapi.responses import JSONResponse
-from worker import create_task
+from worker import celery
 from celery.result import AsyncResult
 
 env_dsn_sentry = os.getenv("SENTRY_PYTHON_DSN", None)
@@ -16,24 +16,37 @@ sentry_sdk.init(
 
 app = FastAPI()
 
+# Health check API
 @app.get("/app/healthcheck")
 def read_root():
     return JSONResponse({"status": "ok"})
 
+# Celery list worker API
+@app.get("/api/workers")
+def get_workers():
+    workers = celery.control.inspect().stats()
+    return JSONResponse(workers)
 
-@app.post("/tasks", status_code=201)
-def run_task(payload=Body(...)):
-    task_type = payload["type"]
-    task = create_task.delay(int(task_type))
+# Celery get registered tasks API
+@app.get("/api/tasks")
+def get_task_lists():
+    tasks = celery.control.inspect().registered()
+    return JSONResponse(tasks)
+
+# Celery get list of active tasks API
+@app.get("/api/active-tasks")
+def get_active_tasks():
+    tasks = celery.control.inspect().active()
+    return JSONResponse(tasks)
+
+# Submit task API
+@app.post("/api/task-submit/{task_name}")
+def submit_task(task_name):
+    task = celery.send_task(task_name)
     return JSONResponse({"task_id": task.id})
 
-
-@app.get("/tasks/{task_id}")
-def get_status(task_id):
-    task_result = AsyncResult(task_id, app=create_task)
-    result = {
-        "task_id": task_id,
-        "task_status": task_result.status,
-        "task_result": task_result.result
-    }
-    return JSONResponse(result)
+# Task status API
+@app.get("/api/task-status/{task_id}")
+def get_task_status(task_id):
+    task = AsyncResult(task_id)
+    return JSONResponse({"task_id": task.id, "task_status": task.status, "task_executed": task.ready(), "task_results": task.result})
